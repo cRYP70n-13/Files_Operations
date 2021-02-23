abdd63fe44f0e4095d6c4c39941d5b02951365ce
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-02-23 14:58:24 +0100

    Fixing the archive

--- archive.go
+++ archive.go
@@ -6,31 +6,30 @@ import (
 	"os"
 )
 
-func errorHandler(err error) {
-	log.Fatal(err)
-}
-
 func main() {
 	// Create a file to write the archive buffer to
 	// Could also use an in memory buffer.
 	outFile, err := os.Create("test.zip")
-	errorHandler(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer outFile.Close()
 
 	// Create a zip writer on top of the file writer
 	zipWriter := zip.NewWriter(outFile)
 
 	// Add files to archive
-	// We use some hard coded data to the purpose of demonstration
-	// but we can also iterate through all the files
-	// in a specific directory and pass the name and contents
-	// of each file, or we can take data from our program
-	// And write it in to the archive
+	// We use some hard coded data to demonstrate,
+	// but you could iterate through all the files
+	// in a directory and pass the name and contents
+	// of each file, or you can take data from your
+	// program and write it write in to the archive
+	// without
 	var filesToArchive = []struct {
 		Name, Body string
 	}{
-		{"test1.txt", "String contents of file"},
-		{"test2.txt", "\x61\x62\x63"},
+		{"test.txt", "String contents of file"},
+		{"test2.txt", "\x61\x62\x63\n"},
 	}
 
 	// Create and write files to the archive, which in turn
@@ -38,13 +37,18 @@ func main() {
 	// .zip file we created at the beginning
 	for _, file := range filesToArchive {
 		fileWriter, err := zipWriter.Create(file.Name)
-		errorHandler(err)
-
+		if err != nil {
+			log.Fatal(err)
+		}
 		_, err = fileWriter.Write([]byte(file.Body))
-		errorHandler(err)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	// Clean our shit up
+	// Clean up
 	err = zipWriter.Close()
-	errorHandler(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
